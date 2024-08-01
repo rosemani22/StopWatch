54876d2c0d4ec9b43dcf990c323805c5412164bb
rosemani22 <rosemani.neemi@gmail.com>
2024-08-01 21:40:28 +0530

    Test commit

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -4,6 +4,7 @@ function Stopwatch()
 {
     const [ timer,setTimer] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
+    
     const formatTime = (seconds)=> {
         const minutes = Math.floor(seconds/60);
         const secs = seconds % 60;
@@ -14,12 +15,9 @@ function Stopwatch()
         let interval;
         if(isRunning)
         {
-            interval=setInterval(()=>{
-                setTimer((prev)=>prev+1);
-            },1000)
-        }else
-        {
-            clearInterval(interval);
+            interval = setInterval(()=>{
+                setTimer((prevTimer)=>prevTimer+1);
+            },100);
         }
         return (()=>{clearInterval(interval);
     })
@@ -31,13 +29,13 @@ function Stopwatch()
         <h2>Timer: {formatTime(timer)}</h2>
         <button onClick={()=>{
             setIsRunning((prev)=> !prev);
-        }}>{isRunning?"Stop":"Start"}</button>
+        }}>{isRunning? "Stop":"Start"}</button>
         <button onClick={()=>{
             setTimer(0);
             setIsRunning(false);
         }}> Reset</button>
         </>
-    )
+    );
 }
 
 export default Stopwatch;
